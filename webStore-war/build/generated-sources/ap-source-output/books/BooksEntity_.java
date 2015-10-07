076b6fb0044cbb8de80a6c0f08c36e962040354b
Rosa Thibault <rosa@b10p10.fil.univ-lille1.fr>
2015-10-07 15:32:59 +0200

    git ignore

--- webStore-war/build/generated-sources/ap-source-output/books/BooksEntity_.java
+++ webStore-war/build/generated-sources/ap-source-output/books/BooksEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-07T14:59:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-07T15:26:17")
 @StaticMetamodel(BooksEntity.class)
 public class BooksEntity_ { 
 
